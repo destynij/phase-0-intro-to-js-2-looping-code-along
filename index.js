1718e0f888ee7328e91ac579c558dec10d39c8a9
Destyni <Destyninjohnson20@icloud.com>
2022-03-19 19:45:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const names = ['Guadalupe', 'Ollie', 'Aki']
+const newMessage = []
+
+function writeCards(names, message) {
+    for (let i = 0; i < names.length; i++) {
+        newMessage.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+    }
+    return newMessage
+}
+
+
+
+function countDown(){
+    let counter = 10;
+    while(counter >=0){
+        console.log(counter)
+        counter = counter - 1;
+    }
+    return counter
+}
+countDown()
